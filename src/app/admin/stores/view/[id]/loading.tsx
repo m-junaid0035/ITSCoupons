f6797f3379246650ddb071fc74f792428e28b568
m-junaid0035 <muhammadjunaid0035@gmail.com>
2025-08-21 23:56:07 +0500

    change loaiding

--- src/app/admin/stores/view/[id]/loading.tsx
+++ src/app/admin/stores/view/[id]/loading.tsx
@@ -8,33 +8,33 @@ export default function StoreViewPageLoading() {
 
       <div className="space-y-6">
         {/* Store Image */}
-        <div>
-          <p className="h-4 w-24 bg-gray-300 dark:bg-gray-700 rounded mb-2"></p>
+        <div className="space-y-2">
+          <div className="h-4 w-24 bg-gray-300 dark:bg-gray-700 rounded"></div>
           <div className="h-48 w-48 bg-gray-300 dark:bg-gray-700 rounded"></div>
         </div>
 
         {/* Store Name */}
-        <div>
-          <p className="h-4 w-32 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
+        <div className="space-y-1">
+          <div className="h-4 w-32 bg-gray-300 dark:bg-gray-700 rounded"></div>
           <div className="h-6 w-64 bg-gray-300 dark:bg-gray-700 rounded"></div>
         </div>
 
         {/* Network Name */}
-        <div>
-          <p className="h-4 w-32 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
+        <div className="space-y-1">
+          <div className="h-4 w-32 bg-gray-300 dark:bg-gray-700 rounded"></div>
           <div className="h-6 w-56 bg-gray-300 dark:bg-gray-700 rounded"></div>
         </div>
 
         {/* Store Network URL */}
-        <div>
-          <p className="h-4 w-36 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
+        <div className="space-y-1">
+          <div className="h-4 w-36 bg-gray-300 dark:bg-gray-700 rounded"></div>
           <div className="h-6 w-72 bg-gray-300 dark:bg-gray-700 rounded"></div>
         </div>
 
         {/* Categories */}
-        <div>
-          <p className="h-4 w-28 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
-          <div className="flex gap-2 flex-wrap">
+        <div className="space-y-1">
+          <div className="h-4 w-28 bg-gray-300 dark:bg-gray-700 rounded"></div>
+          <div className="flex flex-wrap gap-2">
             {[...Array(4)].map((_, i) => (
               <div key={i} className="h-6 w-20 bg-gray-300 dark:bg-gray-700 rounded"></div>
             ))}
@@ -42,20 +42,20 @@ export default function StoreViewPageLoading() {
         </div>
 
         {/* Slug */}
-        <div>
-          <p className="h-4 w-16 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
+        <div className="space-y-1">
+          <div className="h-4 w-16 bg-gray-300 dark:bg-gray-700 rounded"></div>
           <div className="h-6 w-40 bg-gray-300 dark:bg-gray-700 rounded"></div>
         </div>
 
         {/* Total Coupon Used Times */}
-        <div>
-          <p className="h-4 w-48 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
+        <div className="space-y-1">
+          <div className="h-4 w-48 bg-gray-300 dark:bg-gray-700 rounded"></div>
           <div className="h-6 w-16 bg-gray-300 dark:bg-gray-700 rounded"></div>
         </div>
 
         {/* Description */}
-        <div>
-          <p className="h-4 w-24 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
+        <div className="space-y-2">
+          <div className="h-4 w-24 bg-gray-300 dark:bg-gray-700 rounded"></div>
           <div className="space-y-2">
             <div className="h-4 w-full bg-gray-300 dark:bg-gray-700 rounded"></div>
             <div className="h-4 w-5/6 bg-gray-300 dark:bg-gray-700 rounded"></div>
@@ -63,46 +63,28 @@ export default function StoreViewPageLoading() {
           </div>
         </div>
 
-        {/* Meta Title */}
-        <div>
-          <p className="h-4 w-28 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
-          <div className="h-6 w-56 bg-gray-300 dark:bg-gray-700 rounded"></div>
-        </div>
+        {/* SEO Fields */}
+        {["Meta Title", "Meta Description", "Meta Keywords", "Focus Keywords"].map((_, i) => (
+          <div key={i} className="space-y-1">
+            <div className="h-4 w-36 bg-gray-300 dark:bg-gray-700 rounded"></div>
+            <div className="h-6 w-64 bg-gray-300 dark:bg-gray-700 rounded"></div>
+          </div>
+        ))}
 
-        {/* Meta Description */}
-        <div>
-          <p className="h-4 w-36 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
-          <div className="h-6 w-64 bg-gray-300 dark:bg-gray-700 rounded"></div>
-        </div>
-
-        {/* Meta Keywords */}
-        <div>
-          <p className="h-4 w-36 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
-          <div className="h-6 w-48 bg-gray-300 dark:bg-gray-700 rounded"></div>
-        </div>
-
-        {/* Focus Keywords */}
-        <div>
-          <p className="h-4 w-32 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
-          <div className="h-6 w-40 bg-gray-300 dark:bg-gray-700 rounded"></div>
-        </div>
-
-        {/* Popular */}
-        <div>
-          <p className="h-4 w-20 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
-          <div className="h-6 w-24 bg-gray-300 dark:bg-gray-700 rounded"></div>
-        </div>
-
-        {/* Status */}
-        <div>
-          <p className="h-4 w-16 bg-gray-300 dark:bg-gray-700 rounded mb-1"></p>
-          <div className="h-6 w-20 bg-gray-300 dark:bg-gray-700 rounded"></div>
+        {/* Popular & Status */}
+        <div className="flex gap-4">
+          <div className="space-y-1">
+            <div className="h-4 w-20 bg-gray-300 dark:bg-gray-700 rounded"></div>
+            <div className="h-6 w-24 bg-gray-300 dark:bg-gray-700 rounded"></div>
+          </div>
+          <div className="space-y-1">
+            <div className="h-4 w-16 bg-gray-300 dark:bg-gray-700 rounded"></div>
+            <div className="h-6 w-20 bg-gray-300 dark:bg-gray-700 rounded"></div>
+          </div>
         </div>
 
         {/* Created At */}
-        <div>
-          <div className="h-4 w-40 bg-gray-300 dark:bg-gray-700 rounded mt-4"></div>
-        </div>
+        <div className="h-4 w-40 bg-gray-300 dark:bg-gray-700 rounded mt-4"></div>
       </div>
     </div>
   );
