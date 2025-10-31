4e38d33ae4784e2b86a04a708cda5a02c2b82413
m-junaid0035 <muhammadjunaid0035@gmail.com>
2025-10-31 03:06:58 -0700

    final

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -29,7 +29,7 @@ export default function ScrollToTopButton() {
     <button
       onClick={scrollToTop}
       className={`fixed bottom-6 right-6 z-50 p-3 rounded-full shadow-lg transition-all duration-300 
-        bg-purple-600 text-white hover:bg-blue-700 ${
+        bg-purple-600 text-white hover:bg-purple-700 ${
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4 pointer-events-none"
         }`}
     >
